9af6c6e7163a7f45b62c1915f72da732e2bf628f
alex2000d <demarcoalessandro777@gmail.com>
2024-06-24 16:44:36 +0200

    [UPD] changed full_string to string and esercizio finito

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 let first_name = prompt ('inserisci il tuo nome');
 let second_name = prompt ('inserisci il tuo cognome');
 let color = prompt ('inserisci il tuo colore preferito');
-let full_String =  `${first_name} ${second_name} ${color}`;
-console.log(full_String);
+let String =  `${first_name} ${second_name} ${color}`;
+console.log(String);
 document.getElementById('login').innerHTML = `${first_name} ${second_name} ${color} 23`;
