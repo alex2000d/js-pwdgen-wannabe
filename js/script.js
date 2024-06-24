f2838d9c05baa0f69dcbbcba1e33e6a76c46321f
alex2000d <demarcoalessandro777@gmail.com>
2024-06-24 16:24:58 +0200

    [UPD] added the promt name surname and color and inner text in html

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let first_name = prompt ('inserisci il tuo nome');
+let second_name = prompt ('inserisci il tuo cognome');
+let color = prompt ('inserisci il tuo colore preferito');
+let full_String =  `${first_name} ${second_name} ${color}`;
+console.log(full_String);
+document.getElementById('login').innerHTML = 'nomecognomecolorepreferito23' ;
